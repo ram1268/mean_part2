843f0e92a896b06fcbb84f6f78ac1b7452a5c501
ram1268 <ramya33372@gmail.com>
2022-09-30 12:04:29 +0530

    printf_01

---
+++ printf.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+int main()
+{
+    int numOne,numTwo,numThree;
+    numOne=10;
+    numTwo=20;
+    numThree=30;
+    float percentage_0=98.3;
+    double percentage_1=95.55;
+    char mychar='r';
+    short int numSix=50;
+    unsigned int numSeven=100;
+    long int numEight=200;
+    long long int numNine=300;
+    unsigned long int numTen=400;
+    unsigned long long int numEleven=500;
+    signed char mychar_2='R';
+    unsigned char mychar_3='s';
+    long double percentage_3=46.65;
+    
+
+    printf("\nthe numbers are:\n%d\n%d\n%d",numOne,numTwo,numThree);
+    printf("\n the percentage for first is:%f",percentage_0);
+    printf("\n the percentage for second is:%lf",percentage_1);
+    printf("\n mychar is:%c",mychar);
+    printf("\n the num six is:%hd",numSix);
+    printf("\n the number seeventh is:%u",numSeven);
+    printf("\n the eighth number is:%ld",numEight);
+    printf("\n the ninth number is:%lld",numNine);
+    printf("\n the tenth number is:%lu",numTen);
+    printf("\n the eleventh number is:%llu",numEleven);
+    printf("\n the mychar signed char is:%c",mychar_2);
+    printf("\n the unsigned mychar is:%c",mychar_3);
+    printf("\n the long double percentage is:%Lf",percentage_3);
+    
+    
+    
+
+
+
+}
